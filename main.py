bf7e27d6b8cd29d0c21b898aca974d156822c6fa
Sabrinapope.md <40004402+Sabrinapope@users.noreply.github.com>
2021-12-01 22:41:52 -0500

    still working on project

---
+++ main.py
@@ -0,0 +1,29 @@
+i = 1 
+while(i<=10):
+    print(i)
+    i += 1 
+   #
+  
+n = int(input("10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0"))
+i = numbers
+print ("The reversed numbers from {0} to 1 in reverse order: ". format(numbers))
+while (i >= 1):
+    print (1, end = ' ')
+    i = i -1
+    
+    #
+    
+print("Uppercase Alphabets are:")
+for i in range(65,91): 
+        ''' to print alphabets with seperation of space.'''still
+        print(chr(i),end=' ') 
+        
+   #
+   
+max = 100
+num = 1
+
+while num <= max:
+    if(num % 2 == 0):
+        print("{0}" .format(num))
+    num = num + 1
